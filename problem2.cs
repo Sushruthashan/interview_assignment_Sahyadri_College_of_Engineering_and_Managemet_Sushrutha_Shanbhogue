57bf2616a6d4bed6ca1017b174159029b80ebb02
Sushrutha Shanbhogue <137515081+Sushruthashan@users.noreply.github.com>
2025-07-25 15:48:48 +0530

    Update problem2.cs

--- problem2.cs
+++ problem2.cs
@@ -1,1 +1,22 @@
+int n, nums[50], temp;
 
+void zeroes()
+{
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n - i - 1; j++)
+        {
+            if(nums[j] < nums[j+1])
+            {
+                temp = nums[j];
+                nums[j] = nums[j+1];
+                nums[j+1] = temp;
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", nums[i]);
+    }
+}
